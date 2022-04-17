a8b778d29237f6f2e93d57e1b03eeba0e0eaab3c
MoniruzzamanBappy <mdmoniruzzamanbappy@gmail.com>
2022-04-17 19:52:16 +0600

    loading styled

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import { Spinner } from 'react-bootstrap';
+import React from "react";
+import { Button, Spinner } from "react-bootstrap";
+import "./Loading.css";
 
 const Loading = () => {
-    return (
-        <div>
-            <Spinner animation="border" />
-        </div>
-    );
+  return (
+    <div className="loading-com">
+      <Button variant="primary" disabled>
+        <Spinner
+          as="span"
+          animation="border"
+          size="sm"
+          role="status"
+          aria-hidden="true"
+        />
+        <span className="px-3">Loading...</span>
+      </Button>
+    </div>
+  );
 };
 
 export default Loading;
